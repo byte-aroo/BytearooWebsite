387fd27d6b68fa4b743859dbbddc2a1ccc39707a
Akshat Jain <akshat.jain@aurigait.com>
2023-12-20 12:43:25 +0000

    finished till expertise

--- src/Components/Expertise.js
+++ src/Components/Expertise.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import Slide, { Fade } from "react-awesome-reveal";
 import Modal from "react-modal";
 
-
 class Expertise extends Component {
   constructor() {
     super();
@@ -10,6 +9,7 @@ class Expertise extends Component {
       showModal: false,
       selectedExpertise: null,
     };
+    Modal.setAppElement("#root");
   }
 
   openModal = (expertise) => {
@@ -29,6 +29,57 @@ class Expertise extends Component {
     return color;
   }
 
+  knowMoreExpertiseButton(expertise) {
+    return (
+      <button
+        style={{
+          background: "none",
+          border: "none",
+          padding: 0,
+          font: "inherit",
+          cursor: "pointer",
+          textDecoration: "underline",
+          color: "blue",
+          fontWeight: "bold",
+          fontStyle: "italic",
+        }}
+        onMouseOver={(e) => (e.target.style.color = "red")}
+        onMouseOut={(e) => (e.target.style.color = "blue")}
+        onClick={() => this.openModal(expertise)}
+      >
+        Know More....
+      </button>
+    );
+  }
+
+  renderModalContent(content) {
+    return (
+      <div className="nine columns main-col">
+        {content.map((item, index) => (
+          <div key={index}>
+            <h2 style={{ display: "block" }}>{item.heading}</h2>
+            {typeof item.value === "string" ? (
+              <div className="content-wrapper" style={{ display: "block" }}>
+                <p>{item.value}</p>
+              </div>
+            ) : (
+              <div className="sub-heading-wrapper">
+                {item.value.map((subItem, subIndex) => (
+                  <div key={subIndex}>
+                    <h6 style={{ color: "white", textDecoration: "underline" }}>
+                      {subItem.heading}
+                    </h6>
+                    <p>{subItem.value}</p>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+    );
+  }
+
   render() {
     if (!this.props.data) return null;
     const randomChatBotColor = this.getRandomColor();
@@ -41,13 +92,13 @@ class Expertise extends Component {
             <em className="date">{chatbot.feature}</em>
           </p>
           <p>{chatbot.description}</p>
-          <a onClick={() => this.openModal(chatbot)}>Know More....</a>
+          {this.knowMoreExpertiseButton(chatbot)}
         </div>
       );
     });
 
     const randomWebsiteColor = this.getRandomColor();
-    const website = this.props.data.website.map(function (website) {
+    const website = this.props.data.website.map((website) => {
       return (
         <div key={website.title}>
           <h3>{website.title}</h3>
@@ -56,61 +107,61 @@ class Expertise extends Component {
             <em className="date">{website.feature}</em>
           </p>
           <p>{website.description}</p>
-          <a>Know More....</a>
+          {this.knowMoreExpertiseButton(website)}
         </div>
       );
     });
 
     const randomMobileColor = this.getRandomColor();
-    const mobileApplication = this.props.data.mobileApplication.map(function (
-      mobileApplication
-    ) {
-      return (
-        <div key={mobileApplication.title}>
-          <h3>{mobileApplication.title}</h3>
-          <p className="info" style={{ color: randomMobileColor }}>
-            {mobileApplication.subHeading} <span>&bull;</span>
-            <em className="date">{mobileApplication.feature}</em>
-          </p>
-          <p>{mobileApplication.description}</p>
-          <a>Know More....</a>
-        </div>
-      );
-    });
+    const mobileApplication = this.props.data.mobileApplication.map(
+      (mobileApplication) => {
+        return (
+          <div key={mobileApplication.title}>
+            <h3>{mobileApplication.title}</h3>
+            <p className="info" style={{ color: randomMobileColor }}>
+              {mobileApplication.subHeading} <span>&bull;</span>
+              <em className="date">{mobileApplication.feature}</em>
+            </p>
+            <p>{mobileApplication.description}</p>
+            {this.knowMoreExpertiseButton(mobileApplication)}
+          </div>
+        );
+      }
+    );
 
     const randomSystemColor = this.getRandomColor();
-    const systemSoftware = this.props.data.systemSoftware.map(function (
-      systemSoftware
-    ) {
-      return (
-        <div key={systemSoftware.title}>
-          <h3>{systemSoftware.title}</h3>
-          <p className="info" style={{ color: randomSystemColor }}>
-            {systemSoftware.subHeading} <span>&bull;</span>
-            <em className="date">{systemSoftware.feature}</em>
-          </p>
-          <p>{systemSoftware.description}</p>
-          <a>Know More....</a>
-        </div>
-      );
-    });
+    const systemSoftware = this.props.data.systemSoftware.map(
+      (systemSoftware) => {
+        return (
+          <div key={systemSoftware.title}>
+            <h3>{systemSoftware.title}</h3>
+            <p className="info" style={{ color: randomSystemColor }}>
+              {systemSoftware.subHeading} <span>&bull;</span>
+              <em className="date">{systemSoftware.feature}</em>
+            </p>
+            <p>{systemSoftware.description}</p>
+            {this.knowMoreExpertiseButton(systemSoftware)}
+          </div>
+        );
+      }
+    );
 
     const randomManagementColor = this.getRandomColor();
-    const managementSoftware = this.props.data.managementSoftware.map(function (
-      managementSoftware
-    ) {
-      return (
-        <div key={managementSoftware.title}>
-          <h3>{managementSoftware.title}</h3>
-          <p className="info" style={{ color: randomManagementColor }}>
-            {managementSoftware.subHeading} <span>&bull;</span>
-            <em className="date">{managementSoftware.feature}</em>
-          </p>
-          <p>{managementSoftware.description}</p>
-          <a>Know More....</a>
-        </div>
-      );
-    });
+    const managementSoftware = this.props.data.managementSoftware.map(
+      (managementSoftware) => {
+        return (
+          <div key={managementSoftware.title}>
+            <h3>{managementSoftware.title}</h3>
+            <p className="info" style={{ color: randomManagementColor }}>
+              {managementSoftware.subHeading} <span>&bull;</span>
+              <em className="date">{managementSoftware.feature}</em>
+            </p>
+            <p>{managementSoftware.description}</p>
+            {this.knowMoreExpertiseButton(managementSoftware)}
+          </div>
+        );
+      }
+    );
 
     return (
       <section id="resume">
@@ -185,29 +236,39 @@ class Expertise extends Component {
         >
           {this.state.selectedExpertise && (
             <div>
-              <h2>{this.state.selectedExpertise.title}</h2>
-              {/* <p>{this.state.selectedExpertise.description}</p> */}
               <section id="about">
                 <Fade duration={1000}>
                   <div className="row">
                     <div className="three columns">
                       <img
                         className="header-logo"
-                        src="images/BytearooLogo.png"
+                        src={this.state.selectedExpertise.modal.image}
                         alt="Bytearoo Logo"
                       />
                     </div>
-                    <div className="nine columns main-col">
-                      <h2>About Us</h2>
-                      <p>
-                        {this.state.selectedExpertise.modal}
-                      </p>
-                      <h6 style={{ color: "white" }}>"this is sub bio"</h6>
-                    </div>
+                    {this.renderModalContent(
+                      this.state.selectedExpertise.modal.content
+                    )}
                   </div>
+                  <p
+                    style={{
+                      textAlign: "center",
+                      padding: "0 20px",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    ~ {this.state.selectedExpertise.modal.finalMessage}
+                  </p>
                 </Fade>
               </section>
-              <button onClick={this.closeModal}>Close</button>
+              <div style={{ textAlign: "center", background: "black" }}>
+                <button
+                  style={{ margin: "10px 0", borderRadius: "5px" }}
+                  onClick={this.closeModal}
+                >
+                  Close
+                </button>
+              </div>
             </div>
           )}
         </Modal>
